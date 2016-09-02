e964d612005a50ddfeea379afcc0ed4c8712f38b
dmytroPPK <magnazer@gmail.com>
2016-09-02 23:14:44 +0300

    Init project!

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Home works are coming soon!!!!");
+        }
+    }
+}
